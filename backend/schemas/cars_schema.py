50ed3f7a440f76700980e6caec751afe24f5f722
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 16:01:36 +0300

    Created the MonfoBaseModel class

--- backend/schemas/cars_schema.py
+++ backend/schemas/cars_schema.py
@@ -1,1 +1,9 @@
 """The schema file for the cars being sold"""
+from pydantic import BaseModel, Field
+
+
+class MongoBaseModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+
+    class Config:
+        json_encoders = {ObjectId: str}
