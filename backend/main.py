e2bc2564208e4d7a83a3b535e20f038ac45b9a25
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 16:29:44 +0300

    Naendelea kesho

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(DB_URL)
+
 @app.get("/")
 async def root() -> dict:
     return {'Message': 'This API works'}
