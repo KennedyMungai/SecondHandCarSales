ff851d68d852aef33cf7b48509bd0d75e777c144
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 16:03:34 +0300

    Created the CarBase class that inherits the CarBase class

--- backend/schemas/cars_schema.py
+++ backend/schemas/cars_schema.py
@@ -7,3 +7,12 @@ class MongoBaseModel(BaseModel):
 
     class Config:
         json_encoders = {ObjectId: str}
+
+
+class CarBase(MongoBaseModel):
+    brand: str = Field(..., min_length=3)
+    make: str = Field(..., min_length=3)
+    year: int = Field(...)
+    price: int = Field(...)
+    km: int = Field(...)
+    cm3: int = Field(...)
