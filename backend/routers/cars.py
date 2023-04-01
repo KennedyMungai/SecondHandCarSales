f054ee2b5c30d5c235169d88665c344a52db3ea0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-01 11:55:27 +0300

    Created a new post endpoint for creating cars

--- backend/routers/cars.py
+++ backend/routers/cars.py
@@ -1,6 +1,9 @@
 """The cars router"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
+from schemas.cars_schema import CarBase
 
 cars_router = APIRouter(prefix="/cars", tags=["Cars"])
 
@@ -13,3 +16,12 @@ async def list_cars():
         dict: A dictionary to show the data to be returned
     """
     return {"data": "All cars will go here"}
+
+
+@cars_router.post("/", response_description="Add a new car")
+async def create_car(_request: Request, _car: CarBase = Body(...)):
+    _car = jsonable_encoder(_car)
+    _new_car = await _request.app.mongodb["cars1"].insert_one(_car)
+    _created_car = await _request.app.mongodb["cars1"].find_one({"_id": _new_car.inserted_id})
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=_created_car)
