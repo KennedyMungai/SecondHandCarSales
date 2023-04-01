be1f2d93433a1709e01769d2e0d81f2949b57926
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-01 11:46:50 +0300

    Added a docstring to teh list_cars endpoint

--- backend/routers/cars.py
+++ backend/routers/cars.py
@@ -7,4 +7,9 @@ cars_router = APIRouter(prefix="/cars", tags=["Cars"])
 
 @cars_router.get("/", response_description="List all the cats")
 async def list_cars():
+    """The root endpoint for the cars endpoint
+
+    Returns:
+        dict: A dictionary to show the data to be returned
+    """
     return {"data": "All cars will go here"}
