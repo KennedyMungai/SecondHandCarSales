410e2231036c259f065ffcfabdf38140be861716
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-01 11:46:02 +0300

    Added the root car endpoint to the cars python file

--- backend/routers/cars.py
+++ backend/routers/cars.py
@@ -3,3 +3,8 @@ from fastapi import APIRouter
 
 
 cars_router = APIRouter(prefix="/cars", tags=["Cars"])
+
+
+@cars_router.get("/", response_description="List all the cats")
+async def list_cars():
+    return {"data": "All cars will go here"}
